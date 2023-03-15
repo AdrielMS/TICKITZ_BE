a730273e5f68c8158a5144e9da93609e53a55dc9
AdrielMS <adrielsjahranie@gmail.com>
2023-03-15 18:10:28 +0700

    Update: Read Database from Supabase

--- index.js
+++ index.js
@@ -1,50 +1,44 @@
-
 require("dotenv").config();
 //import eskternal
-const { urlencoded, json } = require('express');
-const express = require('express')
-const app = express()
+const { urlencoded, json } = require("express");
+const express = require("express");
+const app = express();
 // const { v4: uuidv4 } = require('uuid');
-const cors = require('cors')
+const cors = require("cors");
 
 //port
 const port = 5000;
 
 //import internal
-const db = require('./helper/connection')
-const router = require('./src/routes/index')
+const db = require("./helper/connection");
+const router = require("./src/routes/index");
 
 //menerima x-www.form.urlencoded
-app.use(urlencoded( {extended: true} ))
-
+app.use(urlencoded({ extended: true }));
 
 //static file
-app.use(express.static("public"))
+app.use(express.static("public"));
+app.use(express.static("src"));
 
 //menerima raw JSON
-app.use(json())
+app.use(json());
 
 //cors
-app.use(cors()) //semua bisa akses
+app.use(cors()); //semua bisa akses
 
 //routes parent
-app.use('/api/v1/', router)
-
+app.use("/api/v1/", router);
 
 //endpoint
 //routing if can't get a routes
-app.get('*', (req, res) => {
-    return res.send({
-        status: 404,
-        message: "not found"
-    })
-})
+app.get("*", (req, res) => {
+  return res.send({
+    status: 404,
+    message: "not found",
+  });
+});
 
 //listening server~
 app.listen(port, (req, res) => {
-    console.log(`backend successfully running on port ${port}`);
-})
-
-
-
-
+  console.log(`backend successfully running on port ${port}`);
+});
